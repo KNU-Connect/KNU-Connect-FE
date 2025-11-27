fd0ba024531b2539d5229b4d26131dca058fd9b6
hoon_ground <break_the_limit@naver.com>
2025-11-28 03:37:42 +0900

    chore : 개발 서버 proxy 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_PROXY_TARGET,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
