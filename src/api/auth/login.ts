23d045df051a8a2c7e238c47f5cd40d2b8994fcb
hoon_ground <break_the_limit@naver.com>
2025-11-28 03:13:18 +0900

    feat : 로그인 API 구현, accessToken 저장 기능 추가

---
+++ src/api/auth/login.ts
@@ -0,0 +1,32 @@
+import { useMutation } from '@tanstack/react-query';
+import axiosInstance from '../axiosInstance';
+import { useAuthStore } from '@/store/authStore';
+
+export interface LoginRequestBody {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponseBody {
+  token: string;
+}
+
+export async function login(requestBody: LoginRequestBody) {
+  const { data: response } = await axiosInstance.post<LoginResponseBody>(
+    '/auth/login',
+    requestBody,
+  );
+
+  return response;
+}
+
+export function useLogin() {
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
+
+  return useMutation({
+    mutationFn: (requestBody: LoginRequestBody) => login(requestBody),
+    onSuccess: (data) => {
+      setAccessToken(data.token);
+    },
+  });
+}
